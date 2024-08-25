6c624e3e0cd5aed6cbdc5d73ff69fb4276a07d3f
Leslie Arkorful <lesliearkorful@gmail.com>
2024-08-25 04:16:46 +0000

    Create account_type_view.dart

---
+++ lib/ui/index/account_type_view.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tugela/models/company.dart';
+import 'package:tugela/models/freelancer.dart';
+import 'package:tugela/models/user.dart';
+import 'package:tugela/providers/company_provider.dart';
+import 'package:tugela/providers/user_provider.dart';
+
+class AccountTypeView extends StatelessWidget {
+  final Widget Function(
+    BuildContext context,
+    Freelancer? freelancer,
+  )? freelancer;
+  final Widget Function(
+    BuildContext context,
+    Company? company,
+  )? company;
+  final EdgeInsets? padding;
+  const AccountTypeView({
+    super.key,
+    this.company,
+    this.freelancer,
+    this.padding,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final userProvider = context.watch<UserProvider>();
+    final companyProvider = context.watch<CompanyProvider>();
+    return Padding(
+      padding: padding ?? EdgeInsets.zero,
+      child: (userProvider.user?.accountType == AccountType.company
+              ? company?.call(
+                  context,
+                  companyProvider.company[userProvider.user?.company?.id] ??
+                      userProvider.user?.company)
+              : freelancer?.call(context, userProvider.user?.freelancer)) ??
+          const SizedBox.shrink(),
+    );
+  }
+}
