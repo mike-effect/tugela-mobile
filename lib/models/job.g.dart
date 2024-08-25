f6b304996b4638cb160071edb9751062a4f4d2cd
Leslie Arkorful <lesliearkorful@gmail.com>
2024-08-25 04:16:21 +0000

    Update job.g.dart

--- lib/models/job.g.dart
+++ lib/models/job.g.dart
@@ -9,20 +9,31 @@ part of 'job.dart';
 Job _$JobFromJson(Map<String, dynamic> json) => Job(
       id: json['id'] as String?,
       title: json['title'] as String?,
-      company: json['company'] as String?,
+      company: json['company'] == null
+          ? null
+          : Company.fromJson(json['company'] as Map<String, dynamic>),
       description: json['description'] as String?,
+      experience: json['experience'] as String?,
+      responsibilities: json['responsibilities'] as String?,
       date:
           json['date'] == null ? null : DateTime.parse(json['date'] as String),
-      location: json['location'] as String?,
+      location: $enumDecodeNullable(_$JobLocationEnumMap, json['location'],
+          unknownValue: JsonKey.nullForUndefinedEnumValue),
       address: json['address'] as String?,
       tags:
           (json['tags'] as List<dynamic>?)?.map((e) => e as String).toList() ??
               const [],
-      priceType: json['price_type'] as String?,
+      priceType: $enumDecodeNullable(_$PriceTypeEnumMap, json['price_type'],
+          unknownValue: JsonKey.nullForUndefinedEnumValue),
       price: json['price'] as String?,
+      minPrice: json['min_price'] as String?,
+      maxPrice: json['max_price'] as String?,
       currency: json['currency'] as String?,
-      applicationType: json['application_type'] as String?,
-      status: json['status'] as String?,
+      applicationType: $enumDecodeNullable(
+          _$JobApplicationTypeEnumMap, json['application_type'],
+          unknownValue: JsonKey.nullForUndefinedEnumValue),
+      status: $enumDecodeNullable(_$JobStatusEnumMap, json['status'],
+          unknownValue: JsonKey.nullForUndefinedEnumValue),
       externalApplyLink: json['external_apply_link'] as String?,
       createdAt: json['created_at'] == null
           ? null
@@ -30,6 +41,8 @@ Job _$JobFromJson(Map<String, dynamic> json) => Job(
       updatedAt: json['updated_at'] == null
           ? null
           : DateTime.parse(json['updated_at'] as String),
+      roleType: $enumDecodeNullable(_$JobRoleTypeEnumMap, json['role_type'],
+          unknownValue: JsonKey.nullForUndefinedEnumValue),
     );
 
 Map<String, dynamic> _$JobToJson(Job instance) {
@@ -43,19 +56,59 @@ Map<String, dynamic> _$JobToJson(Job instance) {
 
   writeNotNull('id', instance.id);
   writeNotNull('title', instance.title);
-  writeNotNull('company', instance.company);
+  writeNotNull('company', instance.company?.toJson());
   writeNotNull('description', instance.description);
+  writeNotNull('experience', instance.experience);
+  writeNotNull('responsibilities', instance.responsibilities);
   writeNotNull('date', instance.date?.toIso8601String());
-  writeNotNull('location', instance.location);
+  writeNotNull('location', _$JobLocationEnumMap[instance.location]);
   writeNotNull('address', instance.address);
   val['tags'] = instance.tags;
-  writeNotNull('price_type', instance.priceType);
+  writeNotNull('price_type', _$PriceTypeEnumMap[instance.priceType]);
   writeNotNull('price', instance.price);
+  writeNotNull('max_price', instance.maxPrice);
+  writeNotNull('min_price', instance.minPrice);
   writeNotNull('currency', instance.currency);
-  writeNotNull('application_type', instance.applicationType);
-  writeNotNull('status', instance.status);
+  writeNotNull('application_type',
+      _$JobApplicationTypeEnumMap[instance.applicationType]);
+  writeNotNull('status', _$JobStatusEnumMap[instance.status]);
+  writeNotNull('role_type', _$JobRoleTypeEnumMap[instance.roleType]);
   writeNotNull('external_apply_link', instance.externalApplyLink);
   writeNotNull('created_at', instance.createdAt?.toIso8601String());
   writeNotNull('updated_at', instance.updatedAt?.toIso8601String());
   return val;
 }
+
+const _$JobLocationEnumMap = {
+  JobLocation.onsite: 'on-site',
+  JobLocation.remote: 'remote',
+  JobLocation.hybrid: 'hybrid',
+};
+
+const _$PriceTypeEnumMap = {
+  PriceType.perProject: 'per_project',
+  PriceType.daily: 'per_day',
+  PriceType.hourly: 'per_hour',
+  PriceType.weekly: 'per_week',
+  PriceType.monthly: 'per_month',
+  PriceType.yearly: 'per_year',
+};
+
+const _$JobApplicationTypeEnumMap = {
+  JobApplicationType.internal: 'internal',
+  JobApplicationType.external: 'external',
+};
+
+const _$JobStatusEnumMap = {
+  JobStatus.active: 'active',
+  JobStatus.inactive: 'inactive',
+  JobStatus.assigned: 'assigned',
+  JobStatus.completed: 'completed',
+};
+
+const _$JobRoleTypeEnumMap = {
+  JobRoleType.partTime: 'part_time',
+  JobRoleType.contract: 'contract',
+  JobRoleType.fullTime: 'full_time',
+  JobRoleType.internship: 'internship',
+};
