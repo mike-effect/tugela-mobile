4914affd01cd10900e3414b369584e4cc62cd4be
Leslie Arkorful <lesliearkorful@gmail.com>
2024-08-25 04:16:27 +0000

    Update user.g.dart

--- lib/models/user.g.dart
+++ lib/models/user.g.dart
@@ -9,12 +9,20 @@ part of 'user.dart';
 User _$UserFromJson(Map<String, dynamic> json) => User(
       email: json['email'] as String?,
       username: json['username'] as String?,
-      accountType: json['account_type'] as String?,
+      accountType: $enumDecodeNullable(
+          _$AccountTypeEnumMap, json['account_type'],
+          unknownValue: JsonKey.nullForUndefinedEnumValue),
       role: json['role'] as String?,
       id: json['id'] as String?,
       profile: json['profile'] == null
           ? null
           : Profile.fromJson(json['profile'] as Map<String, dynamic>),
+      company: json['company'] == null
+          ? null
+          : Company.fromJson(json['company'] as Map<String, dynamic>),
+      freelancer: json['freelancer'] == null
+          ? null
+          : Freelancer.fromJson(json['freelancer'] as Map<String, dynamic>),
     );
 
 Map<String, dynamic> _$UserToJson(User instance) {
@@ -28,9 +36,17 @@ Map<String, dynamic> _$UserToJson(User instance) {
 
   writeNotNull('email', instance.email);
   writeNotNull('username', instance.username);
-  writeNotNull('account_type', instance.accountType);
+  writeNotNull('account_type', _$AccountTypeEnumMap[instance.accountType]);
   writeNotNull('role', instance.role);
   writeNotNull('id', instance.id);
   writeNotNull('profile', instance.profile?.toJson());
+  writeNotNull('freelancer', instance.freelancer?.toJson());
+  writeNotNull('company', instance.company?.toJson());
   return val;
 }
+
+const _$AccountTypeEnumMap = {
+  AccountType.company: 'company',
+  AccountType.freelancer: 'freelancer',
+  AccountType.unknown: 'unknown',
+};
