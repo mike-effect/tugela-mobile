99c8da9aca373a0387b7239fddb44529eea8882c
Leslie Arkorful <lesliearkorful@gmail.com>
2024-08-25 04:16:26 +0000

    Update user.dart

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,5 +1,7 @@
 import 'package:json_annotation/json_annotation.dart';
 import 'package:tugela/models/base_model.dart';
+import 'package:tugela/models/company.dart';
+import 'package:tugela/models/freelancer.dart';
 import 'package:tugela/models/profile.dart';
 
 part 'user.g.dart';
@@ -12,17 +14,21 @@ enum HowYouFoundUs {
   techEvent,
   afriblockMember,
   afriblockEmployee,
-  unknown,
 }
+
+enum AccountType { company, freelancer, unknown }
 
 @JsonSerializable()
 class User extends BaseModel {
   final String? email;
   final String? username;
-  final String? accountType;
+  @JsonKey(unknownEnumValue: JsonKey.nullForUndefinedEnumValue)
+  final AccountType? accountType;
   final String? role;
   final String? id;
   final Profile? profile;
+  final Freelancer? freelancer;
+  final Company? company;
 
   const User({
     this.email,
@@ -31,6 +37,8 @@ class User extends BaseModel {
     this.role,
     this.id,
     this.profile,
+    this.company,
+    this.freelancer,
   });
 
   factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
