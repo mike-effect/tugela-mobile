a6b34a09f55e16c98d7339e947414a413f8669d4
Leslie Arkorful <lesliearkorful@gmail.com>
2024-08-25 04:16:22 +0000

    Create job_application.g.dart

---
+++ lib/models/job_application.g.dart
@@ -0,0 +1,39 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'job_application.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+JobApplication _$JobApplicationFromJson(Map<String, dynamic> json) =>
+    JobApplication(
+      id: json['id'] as String?,
+      freelancer: json['freelancer'] as String?,
+      job: json['job'] as String?,
+      status: json['status'] as String?,
+      createdAt: json['created_at'] == null
+          ? null
+          : DateTime.parse(json['created_at'] as String),
+      updatedAt: json['updated_at'] == null
+          ? null
+          : DateTime.parse(json['updated_at'] as String),
+    );
+
+Map<String, dynamic> _$JobApplicationToJson(JobApplication instance) {
+  final val = <String, dynamic>{};
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('id', instance.id);
+  writeNotNull('freelancer', instance.freelancer);
+  writeNotNull('job', instance.job);
+  writeNotNull('status', instance.status);
+  writeNotNull('created_at', instance.createdAt?.toIso8601String());
+  writeNotNull('updated_at', instance.updatedAt?.toIso8601String());
+  return val;
+}
