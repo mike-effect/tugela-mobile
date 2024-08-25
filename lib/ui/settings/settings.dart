ce4c057bc84864a5db3680cc0e25ead02cb75455
Leslie Arkorful <lesliearkorful@gmail.com>
2024-08-25 04:16:55 +0000

    Create settings.dart

---
+++ lib/ui/settings/settings.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:tugela/providers/user_provider.dart';
+import 'package:tugela/ui/company/company_create.dart';
+import 'package:tugela/utils.dart';
+import 'package:tugela/widgets/layout/menu_list_tile.dart';
+import 'package:tugela/widgets/layout/sliver_scaffold.dart';
+
+class Settings extends StatelessWidget {
+  const Settings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final userProvider = context.watch<UserProvider>();
+    return SliverScaffold(
+      pageAppBar: const PageAppBar(
+        largeTitle: Text("Settings"),
+      ),
+      bodyPadding: ContentPadding,
+      body: Column(
+        children: [
+          MenuListTile(
+            iconData: PhosphorIconsRegular.user,
+            title: "Edit Profile",
+            onTap: () {
+              push(
+                context: context,
+                builder: (_) => CompanyCreate(
+                  company: userProvider.user?.company,
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: OutlinedButton(
+          onPressed: () {
+            userProvider.logout();
+          },
+          child: const Text("Log out"),
+        ),
+      ),
+    );
+  }
+}
