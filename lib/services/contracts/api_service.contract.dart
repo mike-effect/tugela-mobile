5c4e37af5b4986c6904cc701d7c24dfad371adea
Leslie Arkorful <lesliearkorful@gmail.com>
2024-08-25 04:16:37 +0000

    Update api_service.contract.dart

--- lib/services/contracts/api_service.contract.dart
+++ lib/services/contracts/api_service.contract.dart
@@ -62,20 +62,112 @@ abstract class ApiServiceContract {
     Map<String, dynamic> params = const {},
   });
 
-  Future<ApiResponse<Company>> createCompany(Company data);
+  Future<ApiResponse<List<Industry>>> getCompanyIndustries({
+    Map<String, dynamic> params = const {},
+  });
+
+  Future<ApiResponse<List<CompanyValue>>> getCompanyValues({
+    Map<String, dynamic> params = const {},
+  });
+
+  Future<ApiResponse<bool>> createCompany(Company data);
 
   Future<ApiResponse<Company>> getCompany(String id);
 
-  Future<ApiResponse<Company>> updateCompany(String id, Company data);
+  Future<ApiResponse<bool>> updateCompany(String id, Company data);
 
   Future<ApiResponse<bool>> deleteCompany(String id);
 
-  Future<ApiResponse<Freelancer>> createFreelancer(Freelancer data);
+  Future<ApiResponse<bool>> createFreelancer(Freelancer data);
 
   Future<ApiResponse<Freelancer>> getFreelancer(String id);
 
-  Future<ApiResponse<Freelancer>> updateFreelancer(
+  Future<ApiResponse<bool>> updateFreelancer(
     String id,
     Freelancer data,
   );
+
+  Future<ApiResponse<List<Freelancer>>> getFreelancers({
+    Map<String, dynamic> params = const {},
+  });
+
+  Future<ApiResponse<bool>> createJobApplication(JobApplication data);
+
+  Future<ApiResponse<JobApplication>> getJobApplication(
+    String jobApplicationId,
+  );
+
+  Future<ApiResponse<bool>> updateJobApplication(
+    String jobApplicationId,
+    JobApplication data,
+  );
+
+  Future<ApiResponse<bool>> deleteJobApplication(String jobApplicationId);
+
+  Future<ApiResponse<List<JobApplication>>> getJobApplications({
+    String? jobId,
+    String? freelancerId,
+    Map<String, dynamic> params = const {},
+  });
+
+  // Portfolio Item Management
+  Future<ApiResponse<bool>> createPortfolioItem(PortfolioItem data);
+
+  Future<ApiResponse<PortfolioItem>> getPortfolioItem(String portfolioItemId);
+
+  Future<ApiResponse<bool>> updatePortfolioItem(
+    String portfolioItemId,
+    PortfolioItem data,
+  );
+  Future<ApiResponse<bool>> deletePortfolioItem(String portfolioItemId);
+
+  Future<ApiResponse<List<PortfolioItem>>> getPortfolioItems({
+    required String freelancerId,
+    Map<String, dynamic> params = const {},
+  });
+
+  // Service Management
+  Future<ApiResponse<bool>> createService(FreelancerService data);
+
+  Future<ApiResponse<FreelancerService>> getService(String serviceId);
+
+  Future<ApiResponse<bool>> updateService(
+      String serviceId, FreelancerService data);
+
+  Future<ApiResponse<bool>> deleteService(String serviceId);
+
+  Future<ApiResponse<List<FreelancerService>>> getServices({
+    required String freelancerId,
+    Map<String, dynamic> params = const {},
+  });
+
+  // Work Experience Management
+  Future<ApiResponse<bool>> createWorkExperience(WorkExperience data);
+
+  Future<ApiResponse<WorkExperience>> getWorkExperience(String experienceId);
+
+  Future<ApiResponse<bool>> updateWorkExperience(
+    String experienceId,
+    WorkExperience data,
+  );
+
+  Future<ApiResponse<bool>> deleteWorkExperience(String experienceId);
+
+  Future<ApiResponse<List<WorkExperience>>> getWorkExperiences({
+    required String freelancerId,
+    Map<String, dynamic> params = const {},
+  });
+
+  // Job Management
+  Future<ApiResponse<bool>> createJob(Job data);
+
+  Future<ApiResponse<Job>> getJob(String jobId);
+
+  Future<ApiResponse<bool>> updateJob(String jobId, Job data);
+
+  Future<ApiResponse<bool>> deleteJob(String jobId);
+
+  Future<ApiResponse<List<Job>>> getJobs({
+    Map<String, dynamic> params = const {},
+  });
 }
