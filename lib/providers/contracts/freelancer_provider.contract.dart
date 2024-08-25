b036da7cd3ae0c4887ff2ede01dfd7e25159af25
Leslie Arkorful <lesliearkorful@gmail.com>
2024-08-25 04:16:33 +0000

    Update freelancer_provider.contract.dart

--- lib/providers/contracts/freelancer_provider.contract.dart
+++ lib/providers/contracts/freelancer_provider.contract.dart
@@ -4,11 +4,11 @@ import 'package:tugela/providers/base_provider.dart';
 abstract class FreelancerProviderContract extends BaseProvider {
   Freelancer? get profile;
 
-  Future<ApiResponse<Freelancer>?> createFreelancer(Freelancer data);
+  Future<ApiResponse<bool>?> createFreelancer(Freelancer data);
 
   Future<ApiResponse<Freelancer>?> getFreelancer(String id);
 
-  Future<ApiResponse<Freelancer>?> updateFreelancer(
+  Future<ApiResponse<bool>?> updateFreelancer(
     String id,
     Freelancer data,
   );
