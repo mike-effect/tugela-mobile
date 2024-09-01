79dcb59bdee8493ec64fd2d0b06919df3ddefb82
Leslie Arkorful <lesliearkorful@gmail.com>
2024-09-01 16:47:58 +0000

    Update app_provider.dart

--- lib/providers/app_provider.dart
+++ lib/providers/app_provider.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:tugela/models/paginated.dart';
+import 'package:tugela/models/skill.dart';
 import 'package:tugela/providers/contracts/app_provider.contract.dart';
+import 'package:tugela/utils.dart';
 import 'package:tugela/widgets/layout/app_tab_scaffold.dart';
 
 class AppProvider extends AppProviderContract {
   final AppTabController _appTabController = AppTabController();
   ThemeMode _themeMode = ThemeMode.system;
+
+  Paginated<Skill> skills = Paginated();
 
   @override
   AppTabController get appTabController => _appTabController;
@@ -72,4 +77,27 @@ class AppProvider extends AppProviderContract {
 
   @override
   void reset() {}
+
+  Future<Paginated<Skill>?> getSkills({
+    Map<String, dynamic> params = const {},
+    PaginatedOptions options = const PaginatedOptions(),
+  }) async {
+    try {
+      final map = {'page_size': 100, ...params};
+      final res = await paginatedQuery<Skill>(
+        options: options,
+        paginated: skills,
+        initialRequest: () => apiService.getSkills(params: map),
+        loadMoreRequest: () => apiService.getSkills(
+          params: map..addAll({'page': skills.pagination?.next}),
+        ),
+      );
+      if (res?.data != null) skills = res!;
+      notifyListeners();
+      return skills;
+    } catch (e, s) {
+      handleError(e, stackTrace: s);
+      return null;
+    }
+  }
 }
