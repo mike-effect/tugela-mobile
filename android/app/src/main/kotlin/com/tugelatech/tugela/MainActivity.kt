a55f77137db3d7607d34dc9a794e0c8d4a6b399c
Leslie Arkorful <lesliearkorful@gmail.com>
2024-09-01 16:44:55 +0000

    android assets

--- android/app/src/main/kotlin/com/tugelatech/tugela/MainActivity.kt
+++ android/app/src/main/kotlin/com/tugelatech/tugela/MainActivity.kt
@@ -1,5 +1,15 @@
 package com.tugelatech.tugela
 
+import android.os.Build
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+          window.statusBarColor = 0x00000000
+      }
+  }
+}
+
